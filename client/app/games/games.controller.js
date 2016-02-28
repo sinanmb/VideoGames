da6459fd91af7fea2a9e59bf0d641cd6ddd7c9c2
Sinan <sinan@mouline.net>
2016-02-28 15:58:20 -0800

    Allow users to edit and delete games

--- client/app/games/games.controller.js
+++ client/app/games/games.controller.js
@@ -21,5 +21,29 @@ angular.module('videoGamesApp')
         .error (function (err) {
         alert('Error! Something went wrong');
       })
+    };
+
+    $scope.deleteGame = function (index) {
+      $http.delete('/api/games/' + $scope.games[index]._id)
+        .success(function (data) {
+        $scope.games.splice(index, 1);
+      })
+        .error(function (error) {
+        alert('Error! Something went wrong');
+      })
+    };
+
+    $scope.toggleEdit = function (index) {
+      $scope.games[index].edit = !$scope.games[index].edit;
+    };
+
+    $scope.saveGame = function(index) {
+      $http.put('/api/games/' + $scope.games[index]._id, $scope.games[index])
+        .success(function (){
+        $scope.games[index].edit = false;
+      })
+        .error (function (error) {
+        alert('Error! Something went wrong');
+      });
     }
   });
