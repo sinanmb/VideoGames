239e5fc4fadc0ed0ac2e0d734f5beed373d38003
Sinan <sinan@mouline.net>
2016-02-28 15:10:42 -0800

    Initial Scaffolding

---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('videoGamesApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
