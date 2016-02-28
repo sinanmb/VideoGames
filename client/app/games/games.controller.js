70dfaa6502140f8854e1f634c575a47d3b985106
Sinan <sinan@mouline.net>
2016-02-28 15:45:42 -0800

    Allow users to add games

--- client/app/games/games.controller.js
+++ client/app/games/games.controller.js
@@ -1,6 +1,25 @@
 'use strict';
 
 angular.module('videoGamesApp')
-  .controller('GamesCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('GamesCtrl', function ($scope, $http) {
+
+    $http.get('/api/games')
+      .success (function (data) {
+      $scope.games = data;
+      console.log(data);
+    })
+      .error (function(error) {
+      alert('Error! Something went wrong');
+    });
+
+    $scope.addNewGame = function() {
+      $http.post('/api/games', $scope.newGame)
+        .success(function () {
+          $scope.games.push($scope.newGame);
+          $scope.newGame = {};
+        })
+        .error (function (err) {
+        alert('Error! Something went wrong');
+      })
+    }
   });
